6eee6425080c9a8b6f005ff4d73cd9020f2f55c2
Samuel Taylor <samueltaylor@samuels-mbp.home>
2020-12-13 20:06:53 +0000

    code cleanup

--- src/main/kotlin/DayEleven.kt
+++ src/main/kotlin/DayEleven.kt
@@ -2,34 +2,59 @@ import IO.readFile
 import kotlin.math.*
 import kotlin.text.StringBuilder
 
-data class WaitingArea(private var seats: List<String>) {
+const val FLOOR = '.'
+const val EMPTY_SEAT = 'L'
+const val OCCUPIED_SEAT = '#'
 
-    companion object {
+val partOneRule: (Int, Int, List<String>) -> Int = { x, y, data ->
 
-        private const val FLOOR = '.'
-        private const val EMPTY_SEAT = 'L'
-        private const val OCCUPIED_SEAT = '#'
+    var count = 0
+    val startX = max(0, x - 1)
+    val endX = min(x + 1, data[0].length - 1)
+    val startY = max(0, y - 1)
+    val endY = min(y + 1, data.size - 1)
+
+    (startY .. endY).forEach { adjacentY ->
+
+        (startX .. endX).forEach { adjacentX ->
+
+            if (adjacentX != x || adjacentY != y) {
+
+                if (data[adjacentY][adjacentX] == OCCUPIED_SEAT) {
+
+                    count++
+                }
+            }
+        }
     }
+
+    count
+}
+
+data class WaitingArea(private var seats: List<String>) {
 
     val numberOfOccupiedSeats: Long
     get() = seats.sumBy { it.count { seat -> seat == '#' } }.toLong()
 
-    fun tick() = WaitingArea(mutableListOf<String>().apply {
+    fun tick(rule: (Int, Int, List<String>) -> Int) = WaitingArea(mutableListOf<String>().apply {
 
         seats.indices.forEach { y ->
 
             val row = StringBuilder()
             seats[y].indices.forEach { x ->
 
-                row.append(applyRule(x, y, seats))
+                row.append(applyRule(x, y, seats, rule))
             }
             add(row.toString())
         }
     })
 
-    private fun applyRule(x: Int, y: Int, data: List<String>) = when (data[y][x]) {
+    private fun applyRule(x: Int,
+                          y: Int,
+                          data: List<String>,
+                          occupiedSeats: (Int, Int, List<String>) -> Int) = when (data[y][x]) {
 
-        EMPTY_SEAT -> if (occupiedAdjacentSeats(x, y, data) == 0) {
+        EMPTY_SEAT -> if (occupiedSeats(x, y, data) == 0) {
 
             OCCUPIED_SEAT
         } else {
@@ -37,7 +62,7 @@ data class WaitingArea(private var seats: List<String>) {
             EMPTY_SEAT
         }
 
-        OCCUPIED_SEAT -> if (occupiedAdjacentSeats(x, y, data) >= 4) {
+        OCCUPIED_SEAT -> if (occupiedSeats(x, y, data) >= 4) {
 
             EMPTY_SEAT
         } else {
@@ -47,31 +72,6 @@ data class WaitingArea(private var seats: List<String>) {
 
         else -> FLOOR
     }
-
-    private fun occupiedAdjacentSeats(x: Int, y: Int, data: List<String>): Int {
-
-        var count = 0
-        val startX = max(0, x - 1)
-        val endX = min(x + 1, data[0].length - 1)
-        val startY = max(0, y - 1)
-        val endY = min(y + 1, data.size - 1)
-
-        (startY .. endY).forEach { adjacentY ->
-
-            (startX .. endX).forEach { adjacentX ->
-
-                if (adjacentX != x || adjacentY != y) {
-
-                    if (data[adjacentY][adjacentX] == OCCUPIED_SEAT) {
-
-                        count++
-                    }
-                }
-            }
-        }
-
-        return count
-    }
 }
 
 object DayEleven : Day<Long, Long> {
@@ -79,20 +79,23 @@ object DayEleven : Day<Long, Long> {
     override val filename = "/day-eleven.txt"
 
     override val partOneResult: Long
-        get() {
+        get() = settledWaitingArea(waitingArea(), partOneRule).numberOfOccupiedSeats
 
-            lateinit var waitingArea: WaitingArea
-            var newWaitingArea = waitingArea()
-            do {
-
-                waitingArea = newWaitingArea
-                newWaitingArea = waitingArea.tick()
-            } while (waitingArea != newWaitingArea)
-
-            return newWaitingArea.numberOfOccupiedSeats
-        }
     override val partTwoResult: Long
         get() = TODO("Not yet implemented")
 
     private fun waitingArea() = WaitingArea(filename.readFile())
+
+    private fun settledWaitingArea(start: WaitingArea, rule: (Int, Int, List<String>) -> Int): WaitingArea {
+
+        lateinit var waitingArea: WaitingArea
+        var newWaitingArea = start
+        do {
+
+            waitingArea = newWaitingArea
+            newWaitingArea = waitingArea.tick(rule)
+        } while (waitingArea != newWaitingArea)
+
+        return newWaitingArea
+    }
 }
